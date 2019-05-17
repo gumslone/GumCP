4d4629cf9e1042f5c997c6e93d79d78319e1f9d2
gumslone <gumslone@gmx.de>
2019-05-17 18:48:00 +0200

    added bash installer script
    simple bash installer script for apache, php and other modules installation required by GumCP

---
+++ installer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo ""
+echo "GumCP installer"
+echo ""
+sudo apt-get update -y
+echo ""
+echo "Install git"
+echo ""
+sudo apt-get install git -y
+echo ""
+echo "Install apache2"
+echo ""
+sudo apt-get install apache2 -y
+echo ""
+echo "Install mod-php"
+echo ""
+sudo apt-get install php libapache2-mod-php -y
+echo ""
+echo "Install php-sqlite3"
+echo ""
+sudo apt-get install php-sqlite3
+echo ""
+echo "Install php-ssh2"
+echo ""
+sudo apt-get install php-ssh2
+echo ""
+echo "Install wiringPI"
+echo ""
+cd
+sudo git clone git://git.drogon.net/wiringPi
+cd wiringPi
+sudo git pull origin
+./build
+echo ""
+echo "Install GumCP"
+echo ""
+cd /var/www/html
+sudo git clone https://github.com/gumslone/GumCP.git
+sudo chmod 775 -R /var/www/html
+echo ""
+echo "Restart apache service"
+echo ""
+sudo service apache2 restart
+echo ""
+echo "Done"
+echo ""
+
+
+
